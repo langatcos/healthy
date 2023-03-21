61f303a684a058b115e9ee6416647c5ab3b03b64
Cosmas <pkiruicos@gmail.com>
2023-03-21 18:01:55 +0300

    Implemented a crosss origin to allow requests from specific environments

--- src/main/kotlin/com/wellhealth/healthy/controllers/ClaimsDetailsService.kt
+++ src/main/kotlin/com/wellhealth/healthy/controllers/ClaimsDetailsService.kt
@@ -5,18 +5,22 @@ import com.wellhealth.healthy.services.ClaimsDetails
 import com.wellhealth.healthy.services.Users
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class ClaimsDetailsService (val claims:ClaimsDetailsRepository) {
 
     @GetMapping("/getclaimsdetailsbyid/{beneficiaryid}")
     @ApiOperation(value = "Get Claims by Beneficiaryid", notes = "Query all claims by Beneficciaryid")
-
-    fun getclaimsdetailsbyId(@PathVariable beneficiaryid: Int): ResponseEntity<List<ClaimsDetails>> {
+    @CrossOrigin(origins = ["http://localhost:3000"], allowCredentials = "true")
+    fun getclaimsdetailsbyId(@PathVariable beneficiaryid: Int,  response :HttpServletResponse): ResponseEntity<List<ClaimsDetails>> {
         val claimsdetails = claims.getAllclaimsdetailsbyId(beneficiaryid)
+        response.setHeader("Access-Control-Allow-Credentials", "true") // Add the header to the response
+
         return if (claimsdetails != null) {
             ResponseEntity.ok(claimsdetails)
         } else {
